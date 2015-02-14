5ce856dab9d0c8f30aa5db62ccc1039d30fdce01
Todd Harrison <angrymonkeyofdoom@gmail.com>
2015-02-14 12:36:13 -0700

    Modified commands for ease of testing.

--- src/main/java/com/eharrison/canary/r2w/ReturnCommand.java
+++ src/main/java/com/eharrison/canary/r2w/ReturnCommand.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
+import net.canarymod.LineTracer;
 import net.canarymod.api.entity.living.humanoid.Player;
 import net.canarymod.api.world.DimensionType;
 import net.canarymod.api.world.blocks.Block;
@@ -48,7 +49,7 @@ public class ReturnCommand implements CommandListener {
 	}
 	
 	@Command(aliases = {
-			"create", "c"
+		"create"
 	}, parent = "r2w", helpLookup = "r2w create", description = "Create world template", permissions = {
 		"r2w.command.create"
 	}, toolTip = "/r2w create <world_name> <world_dimension>", min = 3, max = 3)
@@ -64,7 +65,7 @@ public class ReturnCommand implements CommandListener {
 	}
 	
 	@Command(aliases = {
-			"remove", "r"
+		"remove"
 	}, parent = "r2w", helpLookup = "r2w remove", description = "Remove world template", permissions = {
 		"r2w.command.remove"
 	}, toolTip = "/r2w remove <world_name> <world_dimension>", min = 3, max = 3)
@@ -83,17 +84,62 @@ public class ReturnCommand implements CommandListener {
 			"restore", "r"
 	}, parent = "r2w", helpLookup = "r2w restore", description = "Restore from template", permissions = {
 		"r2w.command.restore"
-	}, toolTip = "/r2w restore <world_name> <world_dimension> x1 y1 z1 x2 y2 z2", min = 9, max = 9)
+	}, toolTip = "/r2w restore [world_name world_dimension] [x1 y1 z1 [x2 y2 z2]]", min = 1, max = 9)
 	public void restore(final MessageReceiver caller, final String[] parameters)
 			throws InterruptedException, ExecutionException {
-		final int x1 = Integer.parseInt(parameters[3]);
-		final int y1 = Integer.parseInt(parameters[4]);
-		final int z1 = Integer.parseInt(parameters[5]);
-		final int x2 = Integer.parseInt(parameters[6]);
-		final int y2 = Integer.parseInt(parameters[7]);
-		final int z2 = Integer.parseInt(parameters[8]);
-		final Future<Boolean> future = templateManager.restore(parameters[1],
-				DimensionType.fromName(parameters[2]), x1, y1, z1, x2, y2, z2);
+		boolean set = false;
+		String world = null;
+		String dimension = null;
+		int x1 = 0;
+		int y1 = 0;
+		int z1 = 0;
+		int x2 = 0;
+		int y2 = 0;
+		int z2 = 0;
+		
+		if (caller instanceof Player) {
+			final Player player = (Player) caller;
+			if (parameters.length == 1) {
+				final Block block = getBlockLookingAt(player);
+				world = block.getWorld().getName();
+				dimension = block.getWorld().getType().getName();
+				x1 = x2 = block.getX();
+				y1 = y2 = block.getY();
+				z1 = z2 = block.getZ();
+				set = true;
+			} else if (parameters.length == 4) {
+				world = player.getWorld().getName();
+				dimension = player.getWorld().getType().getName();
+				x1 = x2 = Integer.parseInt(parameters[2]);
+				y1 = y2 = Integer.parseInt(parameters[3]);
+				z1 = z2 = Integer.parseInt(parameters[4]);
+				set = true;
+			} else if (parameters.length == 7) {
+				world = player.getWorld().getName();
+				dimension = player.getWorld().getType().getName();
+				x1 = Integer.parseInt(parameters[2]);
+				y1 = Integer.parseInt(parameters[3]);
+				z1 = Integer.parseInt(parameters[4]);
+				x2 = Integer.parseInt(parameters[5]);
+				y2 = Integer.parseInt(parameters[6]);
+				z2 = Integer.parseInt(parameters[7]);
+				set = true;
+			}
+		}
+		
+		if (!set) {
+			world = parameters[1];
+			dimension = parameters[2];
+			x1 = Integer.parseInt(parameters[3]);
+			y1 = Integer.parseInt(parameters[4]);
+			z1 = Integer.parseInt(parameters[5]);
+			x2 = Integer.parseInt(parameters[6]);
+			y2 = Integer.parseInt(parameters[7]);
+			z2 = Integer.parseInt(parameters[8]);
+		}
+		
+		final Future<Boolean> future = templateManager.restore(world,
+				DimensionType.fromName(dimension), x1, y1, z1, x2, y2, z2);
 		if (future.get()) {
 			sendMessage(caller, "Completed restoring template");
 		} else {
@@ -121,6 +167,11 @@ public class ReturnCommand implements CommandListener {
 		} else {
 			sendMessage(caller, "Failed updating template");
 		}
+	}
+	
+	private Block getBlockLookingAt(final Player player) {
+		final LineTracer lineTracer = new LineTracer(player);
+		return lineTracer.getNextBlock();
 	}
 	
 	private void sendMessage(final MessageReceiver caller, final String message) {
